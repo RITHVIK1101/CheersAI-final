c5f46c9c9bb6ddd4b21e0d8f7a05410c5e235930
RITHVIK1101 <rithviksaba@gmail.com>
2024-07-03 13:34:49 -0700

    navbar new

--- src/components/navbar/MenuList.jsx
+++ src/components/navbar/MenuList.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import './Navbar.css';
 import { Menu } from 'antd';
 import { Link } from 'react-router-dom';
@@ -6,10 +6,10 @@ import {
   HomeOutlined,
   AppstoreOutlined,
   AreaChartOutlined,
-  PayCircleOutlined,
   SettingOutlined,
-  BarsOutlined
-} from '@ant-design/icons'
+  BarsOutlined,
+  CalendarOutlined, // Example of different icon for Exercises
+} from '@ant-design/icons';
 
 const MenuList = () => {
   return (
@@ -17,14 +17,12 @@ const MenuList = () => {
       <Menu.Item key="home" icon={<HomeOutlined />}>
         <Link to="/">Home</Link>
       </Menu.Item>
-      <Menu.SubMenu key='tasks' icon={<BarsOutlined />} title="Therapy Suite">
-        <Menu.Item key='Scheduling'>
-          <Link to="/calendar">Scheduling</Link>
-        </Menu.Item>
-        <Menu.Item key='Exercises'>
-          <Link to="/exercises">Exercises</Link>
-        </Menu.Item>
-      </Menu.SubMenu>
+      <Menu.Item key='scheduling' icon={<BarsOutlined />}>
+        <Link to="/calendar">Scheduling</Link>
+      </Menu.Item>
+      <Menu.Item key='exercises' icon={<CalendarOutlined />}>
+        <Link to="/exercises">Exercises</Link>
+      </Menu.Item>
       <Menu.Item key="activity" icon={<AppstoreOutlined />}>
         <Link to="/library">Library</Link>
       </Menu.Item>
@@ -35,7 +33,7 @@ const MenuList = () => {
         <Link to="/settings">Settings</Link>
       </Menu.Item>
     </Menu>
-  )
+  );
 }
 
-export default MenuList
+export default MenuList;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,17 +1,16 @@
 import React, { useState } from 'react';
-import { Button, Layout, theme } from 'antd';
-import { MenuUnfoldOutlined, MenuFoldOutlined } from '@ant-design/icons';
+import { Layout, theme } from 'antd';
 import { Routes, Route, Link } from 'react-router-dom';
-import Logo from './Logo';
 import MenuList from './MenuList';
 import './Navbar.css';
 import Calendar from '../../containers/calendar/Calender'; // Fix the path here
 import Goals from '../../containers/goals/Goals'; // Fix the path here
+import { HeartOutlined } from '@ant-design/icons';
 
-const { Header, Sider, Content } = Layout;
+const { Sider, Content } = Layout;
 
 const Navbar = () => {
-  const [collapsed, setCollapsed] = useState(false);
+  const [collapsed, setCollapsed] = useState(true);
 
   const {
     token: { colorBgContainer },
@@ -30,23 +29,22 @@ const Navbar = () => {
         theme="dark"
         width={300}
         className={`navbar__container ${collapsed ? 'collapsed' : ''}`}
+        onMouseEnter={() => setCollapsed(false)}
+        onMouseLeave={() => setCollapsed(true)}
       >
-        <div className="header-row">
-          {!collapsed && (
-            <Button
-              type="text"
-              className="toggle dark-icon"
-              onClick={toggleCollapsed}
-              icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
-            />
+        <div
+          className="logo__container"
+          onClick={collapsed ? toggleCollapsed : null}
+          style={{ cursor: collapsed ? 'pointer' : 'default' }}
+        >
+          {collapsed ? (
+            <HeartOutlined style={{ fontSize: '24px', color: 'white', margin: '0 auto' }} />
+          ) : (
+            <div className="logo-expanded">
+              <span className="logo-text">CHEERSAI</span>
+              <HeartOutlined style={{ fontSize: '24px', color: 'white', marginLeft: '8px' }} />
+            </div>
           )}
-          <div
-            className="logo__container"
-            onClick={collapsed ? toggleCollapsed : null}
-            style={{ cursor: collapsed ? 'pointer' : 'default' }}
-          >
-            <Logo className="logo-con" />
-          </div>
         </div>
         <MenuList className="menu-bar">
           <Link to="/goals">Goals</Link>
